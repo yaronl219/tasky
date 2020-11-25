cbc825f44dcfd6b52e72a7f6291fda8a24e727b5
Yaron <yaronl219@gmail.com>
2020-11-25 17:58:25 +0200

    edited the about page

--- src/cmps/About.jsx
+++ src/cmps/About.jsx
@@ -14,7 +14,7 @@ export function About({ toggleAbout }) {
                 </div>
                 <Divider />
                 <div className="about-main">
-                    <h3>Tasky was built by Yaron Lipshitz</h3>
+                    <h3>Tasky was built by <a href="https://yaronl219.github.io/portfolio/">Yaron Lipshitz</a></h3>
                     <p>This simple CRUD app was built in order to practice working primarily with MobX, and to work with React beautiful DnD</p>
                     <h4>Tools used in this mini-project</h4>
                     <ul>
@@ -24,9 +24,12 @@ export function About({ toggleAbout }) {
                         <li>React beautiful DnD</li>
                         <li>Node.js</li>
                         <li>MongoDB</li>
+                        <li>Python (for scraping the random todos)</li>
                     </ul>
                     <p>The random task generator was built with tasks from <a href="https://fullylived.com/bucket-list-ideas/">Fully Lived</a>.</p>
                     <p>Check out his website if you're interested in this sort of thing.</p>
+                    <h4>Did you like this project?</h4>
+                    <p>Then take a look at my <a href="https://yaronl219.github.io/portfolio/">portfolio</a></p>
                 </div>
             </div>
         </Paper>
